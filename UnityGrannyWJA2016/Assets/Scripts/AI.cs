be2673dbc9ef5ca5326816682ab8f4003feffc9e
Tinaud <mathieu.naud@hotmail.com>
2016-10-08 02:35:59 -0400

    AI améliorée! :D Les animaux sortent des cages (pour l'instant ils sortent dès leur premier contact avec la porte, il ne faut changer que la veleur random) et les carnivores suivent les herbivores proches et les mangent. De base, les animaux sont en Wander et s'arrêtent de marcher de temps en temps because reasons. Une fois sortis de leur cage, ils ne rentrent plus et ne suivent pas les animaux encore dans leur cage.

--- UnityGrannyWJA2016/Assets/Scripts/AI.cs
+++ UnityGrannyWJA2016/Assets/Scripts/AI.cs
@@ -6,11 +6,11 @@ public class AI : MonoBehaviour {
 
     private const float DRINK_TIME = 2f;
 
-    private Animal animal;
+    private Animal thisAnimal;
     public Animal[] animalList;
     private bool isActive,
                  isEscaped;
-    private enum states { drinking, moving, chasing };
+    private enum states { drinking, wandering, chasing };
     private float moveSpeed;
     private int currentState;
     private GameObject prey;
@@ -18,44 +18,54 @@ public class AI : MonoBehaviour {
                     direction;
 
 	void Start () {
-        animal = GetComponent<Animal>();
-        animalList = transform.root.GetComponentsInChildren<Animal>();
+        thisAnimal = GetComponent<Animal>();
+        //thisAnimal.setId(); //TEMPORAIRE KJDBFOIUSBEGOIUBSGVIOBG
         isActive = true;
         isEscaped = false;
         moveSpeed = 3f;
-        currentState = (int)states.moving;
+        currentState = (int)states.wandering;
         ancientDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
         direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
         StartCoroutine(AnimalStates());
 	}
 	
 	void Update () {
-        switch(currentState) {
-            case (int)states.moving:
+        animalList = transform.root.GetComponentsInChildren<Animal>();
+
+        switch (currentState) {
+            case (int)states.wandering:
                 direction = Wander();
                 transform.Translate(direction * moveSpeed * Time.deltaTime);
                 ancientDirection = direction;
 
-                foreach(Animal a in animalList) {
-                    float distance = GetDistance(a.transform);
-                    if (distance > 1 && distance < 10 && a.GetComponent<AI>().isEscaped) {
-                        prey = a.gameObject;
-                        currentState = (int)states.chasing;
+                if (isEscaped)
+                    foreach(Animal a in animalList) {
+                        if (a.GetComponent<AI>().isEscaped && a.getId() != thisAnimal.getId() && thisAnimal.getType() == false && a.getType() == true) {
+                            float distance = GetDistance(a.transform);
+                            if (distance < 10) {
+                                prey = a.gameObject;
+                                currentState = (int)states.chasing;
+                            }
+                        }
                     }
-                }
-
                 break;
 
             case (int)states.chasing:
-                Debug.Log("BLEBLEBLE");
                 float distancePrey = GetDistance(prey.transform);
-                if (distancePrey < 10)
-                    Seek(prey.transform.position);
-                else if(distancePrey < 1)
-                    currentState = (int)states.moving;
+                if (distancePrey < 1) {
+                    currentState = (int)states.wandering;
+                    Destroy(prey.gameObject);
+                }
+                else if (distancePrey < 10) {
+                    direction = Seek(prey.transform.position);
+                    transform.Translate(direction * moveSpeed * Time.deltaTime);
+                    ancientDirection = direction;
+                }
+                else
+                    currentState = (int)states.wandering;
                 break;
         }
-	}
+    }
 
     Vector2 Wander() {
         direction.x = ancientDirection.x + Random.Range(-0.1f, 0.1f);
@@ -66,9 +76,8 @@ public class AI : MonoBehaviour {
 
     Vector2 Seek(Vector2 pos) {
         Vector2 movingTo;
-        movingTo.x = Mathf.Abs(pos.x - transform.position.x);
-        movingTo.y = Mathf.Abs(pos.y - transform.position.y);
-
+        movingTo.x = pos.x - transform.position.x;
+        movingTo.y = pos.y - transform.position.y;
         return Normalize(movingTo);
     }
 
@@ -87,8 +96,8 @@ public class AI : MonoBehaviour {
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (Random.Range(40, 45) == 42 && !isEscaped && other.name == "Porte") {
-            Debug.Log("Fuck off i'm out");
+        if (/*Random.Range(40, 45)*/ 42 == 42 && !isEscaped && other.tag == "Porte") {
+            Debug.Log("Fuck off i'm out " + thisAnimal.getId());
             isEscaped = true;
             return;
         }
@@ -100,15 +109,10 @@ public class AI : MonoBehaviour {
         while(isActive) {
             yield return new WaitForSeconds(Random.Range(2.0f, 4.0f));
 
-            switch(Random.Range(0, 2)) {
-                case 0:
-                    currentState = (int)states.moving;
-                    break;
-                case 1:
-                    currentState = (int)states.drinking;
-                    yield return new WaitForSeconds(DRINK_TIME);
-                    currentState = (int)states.moving;
-                    break;
+            if(Random.Range(0, 2) == 1 && currentState == (int)states.wandering) {
+                currentState = (int)states.drinking;
+                yield return new WaitForSeconds(DRINK_TIME);
+                currentState = (int)states.wandering;
             }
         }
     }
