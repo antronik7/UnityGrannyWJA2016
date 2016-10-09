4dbe79a8cf5d31c7073c2502c097573825488b47
AntoineDutil <antoineberthiaumedutil@gmail.com>
2016-10-09 11:33:18 -0400

    Scene de fin

---
+++ UnityGrannyWJA2016/Assets/FinalScoreTextController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FinalScoreTextController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        int scoreFinal = 0;
+        GetComponent<Text>().text = scoreFinal.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
