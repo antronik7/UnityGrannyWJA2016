806dcc9e0e7ec2b432100df13923f98658f96c8c
AntoineDutil <antoineberthiaumedutil@gmail.com>
2016-10-08 23:06:58 -0400

    Reparer probleme quand une cage apparait dans les main du gars

--- UnityGrannyWJA2016/Assets/Scripts/Antoine/PlayerController.cs
+++ UnityGrannyWJA2016/Assets/Scripts/Antoine/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour {
     public LayerMask myLayerMask;
     public LayerMask myLayerMaskCage;
     public LayerMask myLayerMaskEntrepot;
-    public GameObject cage;
+    public GameObject cageVictoire;
 
     bool facingRight = true;
     GameObject objetPogner = null;
@@ -43,24 +43,37 @@ public class PlayerController : MonoBehaviour {
 
             if (facingRight)
             {
-                positionCircleCast = 0.25f;
+                positionCircleCast = 0.3f;
             }
             else
             {
-                positionCircleCast = -0.25f;
+                positionCircleCast = -0.3f;
             }
 
             Vector2 originCircleCast = new Vector2(transform.position.x + positionCircleCast, transform.position.y);
 
             if (objetPogner != null)
             {
-                if(objetPogner.tag == "Cage") //DEPOSE ENTREPOT
+                Debug.Log(Input.GetAxis("Vertical"));
+                if (Input.GetAxis("Vertical") > 0.4f)
                 {
+                    positionCircleCast = 0.4f;
+                    originCircleCast = new Vector2(transform.position.x, transform.position.y + positionCircleCast);
+                }
+                else if (Input.GetAxis("Vertical") < -0.4f)
+                {
+                    positionCircleCast = -0.3f;
+                    originCircleCast = new Vector2(transform.position.x, transform.position.y + positionCircleCast);
+                }
+
+                if (objetPogner.tag == "Cage") //DEPOSE ENTREPOT
+                {
+
                     RaycastHit2D hit = Physics2D.CircleCast(originCircleCast, 0.05f, Vector2.right, 0F, myLayerMaskEntrepot);
 
                     if (hit.collider != null)
                     {
-                        DeposeEntrepot(positionCircleCast);
+                        DeposeEntrepot(originCircleCast);
                     }
                 }
                 else
@@ -71,12 +84,12 @@ public class PlayerController : MonoBehaviour {
                     {
                         if (hit.collider.gameObject.GetComponent<CageController>().couleurCage == objetPogner.GetComponent<Animal>().getColor())
                         {
-                            DeposeCage(hit.collider.gameObject, positionCircleCast);
+                            DeposeCage(hit.collider.gameObject, originCircleCast);
                         }
                     }
                     else //DEPOSE A TERRE
                     {
-                        DeposeBateau(positionCircleCast);
+                        DeposeBateau(originCircleCast);
                     }
                 }
             }
@@ -102,6 +115,7 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
+
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         float verticalAxis = Input.GetAxisRaw("Vertical");
 
@@ -155,11 +169,11 @@ public class PlayerController : MonoBehaviour {
         objetPogner.transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, 0);
     }
 
-    void DeposeBateau(float positionCircleCast)
+    void DeposeBateau(Vector3 originCircleCast)
     {
         monAnimator.SetTrigger("Depose");
 
-        objetPogner.transform.position = new Vector3(transform.position.x + positionCircleCast, transform.position.y, 0);
+        objetPogner.transform.position = originCircleCast;
         objetPogner.GetComponent<BoxCollider2D>().enabled = true;
         objetPogner.transform.parent = null;
 
@@ -169,7 +183,7 @@ public class PlayerController : MonoBehaviour {
         objetPogner = null;
     }
 
-    void DeposeCage(GameObject cage, float positionCircleCast)
+    void DeposeCage(GameObject cage, Vector3 originCircleCast)
     {
         monAnimator.SetTrigger("Depose");
 
@@ -177,13 +191,13 @@ public class PlayerController : MonoBehaviour {
         objetPogner.GetComponent<Animal>().setgrabed(false);
 
 
-        objetPogner.transform.position = new Vector3(transform.position.x + positionCircleCast, transform.position.y, 0);
+        objetPogner.transform.position = originCircleCast;
         objetPogner.GetComponent<BoxCollider2D>().enabled = true;
         objetPogner.transform.parent = null;
 
         if (cage.GetComponent<CageController>().setAnimalInCage(objetPogner))
         {
-            objetPogner = Instantiate(cage);
+            objetPogner = Instantiate(cageVictoire);
             objetPogner.transform.parent = transform;
             objetPogner.transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, 0);
             monAnimator.SetTrigger("Porte");
@@ -195,11 +209,11 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void DeposeEntrepot(float positionCircleCast)
+    void DeposeEntrepot(Vector3 originCircleCast)
     {
         monAnimator.SetTrigger("Depose");
 
-        objetPogner.transform.position = new Vector3(transform.position.x + positionCircleCast, transform.position.y, 0);
+        objetPogner.transform.position = originCircleCast;
         objetPogner.GetComponent<BoxCollider2D>().enabled = true;
         objetPogner.transform.parent = null;
 
